7c7286602a6a4ce34a2df328274f8c7f2672cf98
chinsis <gene.u@qq.com>
2025-05-21 17:49:46 +0800

    first commit

---
+++ common/demo.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from enum import Enum
+
+app = FastAPI()
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+def read_user_me():
+    return {"user": "me"}
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    return {"user": user_id} 
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning"}
+    if model_name.value == "resnet":
+        return {"model_name": model_name, "message": "Deep Learning"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "Deep Learning"}
+    return {"model_name": model_name, "message": "Model not found"} 
