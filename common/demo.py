ae94de5e4c73e788dbf3094f458a946cc044b347
chinsis <gene.u@qq.com>
2025-05-22 18:11:03 +0800

    增加query参数方法

--- common/demo.py
+++ common/demo.py
@@ -8,14 +8,18 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
-
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},  
+    {"item_name": "Baz"},
+]
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
 
 @app.get("/users/me")
 def read_user_me():
@@ -34,3 +38,8 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/items")
+async def read_item(skip: int = 0, limit: int = 10):
+    # 返回切片列表
+    return fake_items_db[skip : skip + limit]
