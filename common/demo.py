79f931c3cea1c6644b4b8d772944eddfeee9f553
sukekes <sukekes@gmail.com>
2025-05-24 21:48:39 +0800

    add demo2

--- common/demo.py
+++ common/demo.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -44,20 +45,20 @@ async def itmes(skip: int = 0, limit: int = 10):
     # 返回切片列表
     return fake_items_db[skip : skip + limit]
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str | None=None, short: bool=False):
-    item = {"item_id": item_id}
-    """
-    q是可选参数，可以是string,也可以是None，默认值是None
-    short是bool类型的可选参数，默认值是False，fastapi会自动将其转换为bool类型
-    """
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update({
-            "description": "This is an amazing item that has a long description"
-            })
-    return item
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None=None, short: bool=False):
+#     item = {"item_id": item_id}
+#     """
+#     q是可选参数，可以是string,也可以是None，默认值是None
+#     short是bool类型的可选参数，默认值是False，fastapi会自动将其转换为bool类型
+#     """
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update({
+#             "description": "This is an amazing item that has a long description"
+#             })
+#     return item
 
 @app.get("/users/{user_id}/items/{item_id}")
 def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
@@ -69,3 +70,8 @@ def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None=None):
+    item = {"item_id": item_id, "needy": needy}
+    return item[skip : skip + limit] if limit else item
