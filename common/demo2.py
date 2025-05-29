d1b7452f1f1aaabecf47af35b7f3ccab420a7e3d
chinsis <gene.u@qq.com>
2025-05-29 17:22:44 +0800

    update

--- common/demo2.py
+++ common/demo2.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
-from typing import Union
+from typing import Union, Annotated
 
 
 class Item(BaseModel):
@@ -13,12 +13,15 @@ app = FastAPI()
 
 
 @app.get("/items")
-async def read_item(q: Union[str, None] = Query(default=None, max_length=50)):
+# async def read_item(q: Union[str, None] = Query(default=None, max_length=50)):
+async def read_item(q: Annotated[str | None, Query(max_length=50)] = None):
     """
-    Read an item with the given ID and optional query parameter.
+    Union[str, None] = Query(default=None, max_length=50) 类型声明和规则校验混在一起
+    Annotated[str | None, Query(max_length=50)] = None 类型声明和规则校验分离
     """
     results = {"items": [{"item_id": "Foo"},{"item_id": "Bar"}]}
     if q:
+        # pylance warning:pylance根据resulets推断键值对应该为str：[], update更新的是一个字符串，所以有了错误提示，实际不影响项目运行
         results.update({"q": q})
     return results
 
