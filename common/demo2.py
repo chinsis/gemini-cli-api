e01008f36d6d1019c5fdd1c9c55294851476d921
chinsis <gene.u@qq.com>
2025-05-30 16:03:04 +0800

    add 查询参数模型

--- common/demo2.py
+++ common/demo2.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
-from typing import Union, Annotated, Any
+from fastapi import FastAPI, Query, Path
+from pydantic import BaseModel, Field
+from typing import Union, Annotated, Any, Literal
 
 
 class Item(BaseModel):
@@ -41,10 +41,48 @@ async def create_item(item: Item):
     item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
 
-@app.put("/items/{item_id}")
-@app.post("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+# @app.put("/items/{item_id}")
+# @app.post("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     """
+#     Update an item with the given ID.
+#     """
+#     return {"item_id": item_id, **item.model_dump()}
+
+@app.get("/items/{item_id}")
+# async def read_item_by_id(item_id: Annotated[int, Path(title="The ID of item")], q: Annotated[str | None, Query(max_length=50)] = None):
+async def read_item_by_id(*, q: str, item_id: Annotated[int, Path(title="The ID of item", ge=1, le=1000)]):
     """
-    Update an item with the given ID.
+    Read an item by its ID.
+    带默认值的参数如果放在不带默认值的参数前，python会报错：SyntaxError: parameter without a default follows parameter with a default
+    *可以用来控制参数传递方式，*之后的参数传递时必须使用q=xxx的方式传递，不能直接传递q的值
+    设置校验ge=1, le=1000，表示item_id的值必须在1到1000之间,gt=1, lt=1000表示item_id的值必须在1到1000之间，不包含1和1000
     """
-    return {"item_id": item_id, **item.model_dump()}
+    restults: dict[Any, Any] = {"item_id": item_id}
+    if q:
+        restults.update({"q": q})
+    return restults
+
+
+class FilterParams(BaseModel):
+    """
+    Filter parameters for items.
+    Fields:元数据校验
+    Literal: 限定字段值只能是指定的值
+    """
+    model_config = {
+        "extra": "forbid",  # 禁止传递未定义的字段
+        # "use_enum_values": True,  # 使用枚举值而不是枚举名称
+        # "arbitrary_types_allowed": True,  # 允许任意类型    
+        } 
+    limint: int = Field(10, ge=1, le=100)
+    offset: int = Field(0, ge=0)
+    oder_by: Literal["created_at", "update_at"] = "created_at"
+    tags: list[str] = []
+
+@app.get("/itemes/about")
+async def about(filter_params: Annotated[FilterParams, Query()]):
+    """
+    About the items endpoint.
+    """
+    return filter_params
