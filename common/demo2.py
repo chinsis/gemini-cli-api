633f5217150b08dc125c83cf0950772b24fe0a87
chinsis <gene.u@qq.com>
2025-05-30 11:08:21 +0800

    Query参数使用方法

--- common/demo2.py
+++ common/demo2.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
-from typing import Union, Annotated
+from typing import Union, Annotated, Any
 
 
 class Item(BaseModel):
@@ -14,14 +14,20 @@ app = FastAPI()
 
 @app.get("/items")
 # async def read_item(q: Union[str, None] = Query(default=None, max_length=50)):
-async def read_item(q: Annotated[str | None, Query(max_length=50)] = None):
+async def read_item(q: Annotated[str, Query(max_length=50, min_length=3, pattern="^[A-Za-z]+$", alias="item-query", deprecated=True)]="findindex"):
     """
     Union[str, None] = Query(default=None, max_length=50) 类型声明和规则校验混在一起
     Annotated[str | None, Query(max_length=50)] = None 类型声明和规则校验分离
+    使用Annotated[]="findindex"设置default值，不能使用Annotated[Query(default="findindex")]，两者有冲突
+    Query(pattern="^[A-Za-z]+$", regex="" ) 设置正则表达式校验，其中regex为pydantic v1的正则表达式校验方式，pattern为pydantic v2的正则表达式校验方式，v1已弃用，推荐v2
+    alias="item-query" 设置别名，FastAPI会在文档中显示该别名
+    Deprecated=True 设置该参数为弃用状态，FastAPI会在文档中标记该参数为弃用
     """
-    results = {"items": [{"item_id": "Foo"},{"item_id": "Bar"}]}
+    results: dict[str, Any] = {"items": [{"item_id": "Foo"},{"item_id": "Bar"}]}
+
     if q:
         # pylance warning:pylance根据resulets推断键值对应该为str：[], update更新的是一个字符串，所以有了错误提示，实际不影响项目运行
+        # 如果result声明了类型为dict[str, Any]，则不会再有报错提示
         results.update({"q": q})
     return results
 
